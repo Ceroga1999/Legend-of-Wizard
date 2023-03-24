d993a71b06b57ca272bab64f2fae6d2afbf6cae5
Hitsar <vlex09@ya.ru>
2023-03-24 10:59:14 +0300

    сделал перезарядку

--- Legend of wizard 2.0/Assets/Player/PlayerHealth.cs
+++ Legend of wizard 2.0/Assets/Player/PlayerHealth.cs
@@ -4,15 +4,22 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int _health;
+    private float _timeBetweenAttack = 0.9f;
+
     private HealthBar _healthBar;
 
     private void Start() => _healthBar = GetComponent<HealthBar>();
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Enemy enemy))
         {
-            TakeDamage();
+            _timeBetweenAttack -= Time.deltaTime;
+            if (_timeBetweenAttack <= 0)
+            {
+                TakeDamage();
+                _timeBetweenAttack = 0.9f;
+            }
         }
     }
 
